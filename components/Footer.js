0c94ed298d37b82c79ab9f97098e46f5fe37b8d2
aman-chaudhary-20110012 <109717054+geoportalofindiandams@users.noreply.github.com>
2022-08-09 14:09:12 +0530

    update

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <div className="bg-stone-900 text-gray-100 items-center p-1 ">
-        <h1 className="font-['Open_Sans'] text-center">Made by <a href="https://chaudharyaman.me" target="_blank">@amanchaudhary</a>  </h1>
+        <h1 className="font-['Open_Sans'] text-center">Made by @amanchaudhary </h1>
     </div>
   )
 }
